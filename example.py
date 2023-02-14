b5271f7fa167443b08a2eb3183676e51c3b00413
bigreal106@gmail.com <bigreal106@gmail.com>
2023-02-13 21:01:18 -0500

    operator_type

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ result2 = false or false and 1000 <= 259
 result3 = 10 == 15 and 15 != 10.5 or 567 == "567"
 result4 =  true != false and false == "hola " or hola <= "524"
 result5 = 10 == 15 and 15!= 10.5 or 567 == "567"
-result6 = false <= 8 or 102 >= "8" or hola == "hi"
+result6 = false <= 8 or 102 >= "8" or "hola" == "hi"
 result7 = 7_8999 < 10
 result8 = "aaa" == "aaa"
 result9 = "aaa"!= "bbb"
---
+++ example2.py
@@ -0,0 +1,46 @@
+# = Asigna el valor a la variable
+x = 5  
+print(x)
+
+
+# += Suma el valor a la derecha del igual a la variable X1
+x1 = 5; x1 += 2
+print(x1)
+
+
+# -= Resta el valor a la derecha del igual a la variable X2
+x2 = 5; x2 -= 2
+print(x2)
+
+
+
+# *= Multiplica el valor a la derecha del igual a la variable X3
+x3 = 5; x3 *= 2
+print(x3)
+
+# /= Divide el valor a la derecha del igual a la variable X4
+x4 = 5; x4 /= 2
+print(x4)
+
+
+# %= Devuelve el resto de la división del valor a la derecha del igual con la variable X5
+x5 = 5; x5 %= 2
+print(x5)
+
+
+# //= Devuelve el cociente de la división del valor a la derecha del igual con la variable X6
+x6 = 5; x6 //= 2
+print(x6)
+
+
+#  **= Calcula el exponente a la variable X7 conforme el valor a la derecha del igual
+x7 = 5; x7 **= 2
+print(x7) 
+
+
+
+
+
+
+
+
