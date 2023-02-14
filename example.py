92502fe8827266aea9a1456e129a5c684ef53dac
bigreal106@gmail.com <bigreal106@gmail.com>
2023-02-13 20:34:56 -0500

    Exercise_py

---
+++ example.py
@@ -0,0 +1,19 @@
+true = True
+false = False
+
+result = true == false and 0 < 15 or 70 >= 26
+result1 = false or true and 10_000 != 15
+result2 = false or false and 1000 <= 259
+result3 = 10 == 15 and 15 != 10.5 or 567 == "567"
+result4 =  true != false and false == "hola " or hola <= "524"
+result5 = 10 == 15 and 15!= 10.5 or 567 == "567"
+result6 = false <= 8 or 102 >= "8" or hola == "hi"
+result7 = 7_8999 < 10
+result8 = "aaa" == "aaa"
+result9 = "aaa"!= "bbb"
+result10 = "aaa" == "bbb"
+
+print(result, result1, result2, result3,result4, result5, result6, result7, result8, result9, result10)
+
+
+
